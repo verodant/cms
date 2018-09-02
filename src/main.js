37e864de78e0e17bd02a1b00a254c950578e2135
Verodant <verodant@gmail.com>
2018-09-02 19:27:45 +0200

    delete console

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { Persona } from './model/Persona.js';
 
 const prueba = new Persona();
-console.log(prueba.primaryKey)
 window.prueba = prueba;
 window.Persona = Persona;
 window.a = new Persona(1);
